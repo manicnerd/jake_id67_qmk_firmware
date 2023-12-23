e784793d5f25c9a375f5f5be5850d27132e56d51
Jeff Zeminski <j.zeminski@gmail.com>
2023-12-23 11:29:54 -0500

    figure out keymap issue

--- keyboards/idobao/id67/keymaps/jake/keymap.c
+++ keyboards/idobao/id67/keymaps/jake/keymap.c
@@ -38,6 +38,9 @@ enum {
     #endif  // ID67_DISABLE_UNDERGLOW
 #endif  // RGB_MATRIX_ENABLE
 
+enum custom_keycodes{
+    EMAIL = SAFE_RANGE,
+};
 /* ========== END COPIED FROM IDOBAO KEYMAP ============ */
 
 
@@ -60,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,    KC_E,     KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,    KC_DELETE,
         KC_CAPS,  KC_A,     KC_S,    KC_D,     KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,     KC_PGUP,
         KC_LSFT,            KC_Z,    KC_X,     KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,      KC_PGDN,
-        KC_LCTL,  KC_LALT,           KC_LGUI,                    KC_SPC,                             MO(1),   KC_RCTL,  KC_LEFT,  KC_DOWN,   KC_RIGHT
+        KC_LCTL,  KC_LGUI,           KC_LALT,                    KC_SPC,                             MO(1),   KC_RCTL,  KC_LEFT,  KC_DOWN,   KC_RIGHT
     ),
 
     /*
@@ -86,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_65_ansi_blocker(
         XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   QK_BOOT,
-        XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
+        XXXXXXX,  XXXXXXX,  XXXXXXX, EMAIL  ,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
         XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX,   XXXXXXX,
         XXXXXXX,            XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,
         XXXXXXX,  XXXXXXX,           XXXXXXX,                    XXXXXXX,                            MO(1),   MO(2),    XXXXXXX,  XXXXXXX,   XXXXXXX
@@ -204,7 +207,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case QK_BOOT:
             if (record->event.pressed) {
-                rgb_matrix_set_color_all(RGB_MATRIX_MAXIMUM_BRIGHTNESS, 0, 0);  // All red
+                rgb_matrix_set_color_all(150, 0, 0);  // All red
                 rgb_matrix_driver.flush();
             }
             return true;
@@ -280,6 +283,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         #endif  // RGB_MATRIX_ENABLE
 
+        case EMAIL:
+            if(record->event.pressed) {
+                SEND_STRING("jpz17@pitt.edu");
+            }
+            return false;
 
         default:
             return true; /* Process all other keycodes normally */
